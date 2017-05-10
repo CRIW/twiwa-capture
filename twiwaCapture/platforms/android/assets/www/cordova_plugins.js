dce49f533d2cca788764b40bb1f78af25fa241c6
Valentin Buck <apexys@toppoint.de>
2017-05-10 13:16:22 +0200

    Created cordova project

---
+++ twiwaCapture/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "net.coconauts.notification-listener.NotificationListener",
+        "file": "plugins/net.coconauts.notification-listener/www/notification-listener.js",
+        "pluginId": "net.coconauts.notification-listener",
+        "clobbers": [
+            "notificationListener"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "net.coconauts.notification-listener": "0.0.1"
+};
+// BOTTOM OF METADATA
+});
